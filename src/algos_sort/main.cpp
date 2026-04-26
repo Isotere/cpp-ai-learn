bedd3340bf7e878d65767616daa149c0b600526b
Vyacheslav Tilikov <vyutilikov@avito.ru>
2026-04-26 20:55:17 +0300

    initial for sorting algos

---
+++ src/algos_sort/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from algos_sort\n";
+    return 0;
+}
---
+++ src/algos_sort/tests/test_main.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(algos_sort, SmokeTest) {
+    EXPECT_EQ(1 + 1, 2);
+}
